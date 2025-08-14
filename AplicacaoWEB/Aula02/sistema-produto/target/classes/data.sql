d5039f47f5a586e13504febf2edce22fdbc6c51c
Ana Rúbia de Almeida <134503850+Rubinha14101505@users.noreply.github.com>
2025-08-13 22:51:45 -0300

    Add files via upload

---
+++ AplicacaoWEB/Aula02/sistema-produto/target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (name, price, description) VALUES
+('Notebook', 3500.0, 'Notebook 16G Ram, SSD 512GB'),
+('Smartphone', 1999.0, 'Tela 6.5", 128GB'),
+('Tablet', 1200.0, 'Tela 10", 64GB'),
+('Monitor', 199.0, 'Monitor 24" Full HD 144 Hz'),
+('Teclado', 199.90, 'Teclado Mecanico RGB');
