99475175d70c733da4feb08739dc2ad3502f970a
Rubinha14101505 <anarubiaalmeida1410@gmail.com>
2025-09-05 22:28:25 -0300

    Atividade 01

---
+++ Sistema Aluno/sistema-aluno/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO aluno (nome, email, curso) VALUES 
+('Ana Rubia', 'ana@email.com', 'ADS'),
+('Maria Eduarda', 'maria.edu@email.com', 'Mecatrônica'),
+('João Pereira', 'joaoP@email.com', 'Adm'),
+('Eli', 'eli@email.com', 'Ciência da Computação'),
+('Rebeca', 'beca@email.com', 'Psicologia'),
+('Camilla', 'cami@email.com', 'Robótica');
